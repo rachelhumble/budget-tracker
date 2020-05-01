0c247ebf27a6b55beddd3e3ee2961290d08a3486
Rachel Humble <rachelhumble@me.com>
2020-04-30 23:01:40 -0400

    Heroku deploy bug fix cont'd

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
-const PORT = 7000;
+const PORT = process.env.PORT || 7000;
 
 const app = express();
 
